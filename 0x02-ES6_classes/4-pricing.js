0c46365b39b8760a72d406bcd96dbe2015c95067
Dennis Muchai <dmmuchai@gmail.com>
2025-04-18 02:40:48 +0000

    Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,52 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency object');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setters
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency object');
+    }
+    this._currency = value;
+  }
+
+  // Method: Display full price in specified format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method: Convert amount using conversion rate
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
